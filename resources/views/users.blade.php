e1dfccf131c90c99874c107f7f31cab9d0d50cfa
Najam-Us-Saqib <Saqib92@users.noreply.github.com>
2022-10-10 17:55:39 +0500

    passing data to js

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,13 +1,15 @@
 <x-header componentProps="I am"/>
 <h1> Page view</h1>
 
+@include('contact')
 
+@foreach($names as $n)
+<h4>{{$n}}</h4>
+@endforeach
 
-    @for($i = 0; $i < 10; $i++)
-    <h4>{{$i+1}}</h4>
-    @endfor
+<script>
+    let data = @json($names);
 
+    console.log(data);
 
-    @foreach($names as $n)
-    <h5>{{$n}}</h5>
-    @endforeach
+</script>
